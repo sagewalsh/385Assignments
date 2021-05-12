c232f0dd5fd234ffcef66b0ffbd61a328f05fc30
sagewalsh <sagewalsh@gmail.com>
2021-05-11 18:09:48 -0700

    ConnorUpdate

---
+++ HelloWorld/HelloWorld/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    [SerializeField] public Transform player;
+
+    [SerializeField] public float zoomOut;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, zoomOut);
+        
+    }
+}
