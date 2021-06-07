90bbb118a1693154e5833b3c639b391fd2c61de0
sagewalsh <sagewalsh@gmail.com>
2021-06-07 13:14:07 -0700

    Final v.2

--- HelloWorld/HelloWorld/Assets/Scripts/MainMenu.cs
+++ HelloWorld/HelloWorld/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("FinalScene");
+        SceneManager.LoadScene("FinalSceneWithoutTutorial");
     }
 
     public void PlayGameTutorial()
